b6e8b43d3ef13e601a39183ce16b76cf3b6cf61f
jimmy chirivi <jimmychirivi09@gmail.com>
2020-05-10 14:12:10 -0500

    actualizacion services

--- src/main/java/edu/escuelaing/arep/canciones/CancionesApplication.java
+++ src/main/java/edu/escuelaing/arep/canciones/CancionesApplication.java
@@ -2,27 +2,11 @@ package edu.escuelaing.arep.canciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
-@EnableEurekaClient
 public class CancionesApplication {
 	
-	
-	@Bean
-    @LoadBalanced
-    public RestTemplate getRestTemplate() {
-        HttpComponentsClientHttpRequestFactory httpRequestFactory 
-                = new HttpComponentsClientHttpRequestFactory();
-        httpRequestFactory.setConnectTimeout(10000);
-        return new RestTemplate(httpRequestFactory);
-    }
-
 	
 	
 	public static void main(String[] args) {
